f478f8c8d74b8d62fed1a3ae494bad2f481dfe63
mazenbaddad <mazenbaddad@hotmail.com>
2023-10-30 01:34:46 +0300

    update exe name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "nazeef",
+    name: "nadeef",
     platforms: [
         .macOS(SupportedPlatform.MacOSVersion.v13)
     ],
@@ -15,7 +15,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "nazeef",
+            name: "nadeef",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ],
